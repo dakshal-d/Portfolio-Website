9a327adf3f95f6347640ce0b1ed4f2680ea1652b
Dakshal Dalsania <dakshaldalsania59@gmail.com>
2023-10-04 18:34:21 +0530

    Projects page updated

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -21,15 +21,15 @@ const Projects = () => {
         },
         {
             img: voice,
-            title: "css projects",
-            desc: "Frontend Mentor challange directory, solved with vanilla CSS",
+            title: "Voice Assisstant",
+            desc: "Innovative voice assistant, with advanced natural language processing algorithms, resulting in a significant reduction in manual tasks and enhanced productivity for users",
             live: "https://github.com/dakshal-d/Virtual-Assisstant",
             code: "https://github.com/dakshal-d/Virtual-Assisstant",
         },
         {
             img: Portfolio,
-            title: "get Inspirred",
-            desc: "Quote search app. Used Quotable API for the quotes and React, Redux on the frontend",
+            title: "Portfolio Website",
+            desc: "Personal Portfolio Website. Used React for the development of this Website",
             live: "https://get-inspirred.netlify.app/",
             code: "https://github.com/dakshal-d/Portfolio-Website",
         },
@@ -58,7 +58,7 @@ const Projects = () => {
                         <div className="relative" key={i}>
                             <img className="rounded-lg h-[270px] w-[250px]" src={project.img} alt={project.title} />
                             <div className="flex absolute left-0 right-0 top-[13px] bottom-0 mx-auto w-[90%] h-[85%]  bg-gray-900  opacity-0 duration-500 justify-center flex-col hover:opacity-100 rounded-lg">
-                                <p className="flex py-5 text-center font-bold px-2 text-white">
+                                <p className="flex py-5 text-center mx-auto font-bold px-2 text-white">
                                     {project.desc}
                                 </p>
 
